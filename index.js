f90cefdee19f532ec4983694b8e849f950736c2b
tartor0 <gaaditartor160@gmail.com>
2024-11-13 15:41:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var pi = 3.142
+var radius = 50*50
+var area = pi*radius
+console.log(area)
+
+
+var half = 0.5
+var base = 20
+var height = 25
+var area = half*base*height
+console.log(area)
+
+
+var principal = 10
+var rate = 100
+var time = 60
+var hundred = 100
+var simpleInterest = principal*rate*time/hundred
+console.log(simpleInterest)
+
+
+var b = 20
+var a = 3
+var c = 4
+var four = 4
+var two = 2
+var squareRoot = Math.sqrt(b*b - four*a*c)
+var x = -b - (squareRoot)/two*a  &&  -b + (squareRoot)/two*a
+console.log(x)
